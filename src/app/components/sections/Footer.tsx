a2ecbeaeded6c4150943d839fdb9176728b11a4d
dariovp <dariovp01@gmail.com>
2024-05-31 16:12:49 -0300

    ❤️

--- src/app/components/sections/Footer.tsx
+++ src/app/components/sections/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
 	return (
 		<footer>
 			<h4>
-				© 2024 Copyright, All Right Reserved, Made by Darius & Maisteronix ♥️
+				© 2024 Copyright, All Right Reserved, Made by Darius & Maisteronix ❤️
 			</h4>
 		</footer>
 	)
