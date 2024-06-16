6f17d1559a8fc5f4d95f479f9d940ba9bd7c3217
dariovp <dariovp01@gmail.com>
2024-06-16 18:25:42 -0300

    Migrate hero form to shadcn/ui

--- src/app/components/hero-form.tsx
+++ src/app/components/hero-form.tsx
@@ -1,24 +1,30 @@
+'use client'
 
 import { resolveInput } from "@/actions";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+// import { useFormStatus } from "react-dom";
 
 export function HeroForm() {
+	// const { pending } = useFormStatus();
 	return (
 		<form
-			className="flex flex-col p-5 mt-2 w-full bg-white rounded-xl md:mt-6 lg:mt-10 lg:flex-row"
+			className="flex flex-col items-center p-5 mt-2 w-full bg-white rounded-xl md:mt-6 lg:mt-10 lg:flex-row"
 			action={resolveInput}
 		>
-			<input
+			<Input
 				type="text"
 				placeholder="Address"
 				name="address"
-				className="flex-grow px-2 py-4 w-full bg-white rounded-2xl border border-gray lg:mx-2 lg:w-max"
+				className="flex-grow w-full bg-white rounded-2xl border border-gray lg:mx-2 lg:w-max"
 			/>
-			<button
+			<Button
 				type="submit"
-				className="px-5 py-4 mt-5 w-full text-white bg-red-500 rounded-full lg:mt-0 lg:w-fit"
+				className="mt-5 w-full rounded-full lg:mt-0 lg:w-fit"
+				variant={"destructive"}
 			>
 				Generate Report
-			</button>
+			</Button>
 		</form>
 	)
 }
