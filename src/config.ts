5bd407917b3709225d0d899b836f151acd8cb75e
dariovp <dariovp01@gmail.com>
2024-06-16 17:21:43 -0300

    Add alchemy RPC

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import { mainnet, bsc, arbitrum } from 'wagmi/chains'
 export const config = createConfig({
 	chains: [mainnet, bsc, arbitrum],
 	transports: {
-		[mainnet.id]: http(),
+		[mainnet.id]: http("https://eth-mainnet.g.alchemy.com/v2/h7NRrWwT641myxttbQgAdzfQxoulxefU"),
 		[bsc.id]: http(),
 		[arbitrum.id]: http(),
 	},
