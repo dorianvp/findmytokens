a55d6b7808604629ff6f23b24cdb5534764218d7
dariovp <dariovp01@gmail.com>
2024-06-18 17:59:41 -0300

    Swap h2 with h1 on navbar

--- src/app/components/sections/Hero.tsx
+++ src/app/components/sections/Hero.tsx
@@ -23,16 +23,16 @@ export function Hero() {
 				className="object-fill absolute -left-0 -bottom-1 w-full -z-10"
 			/>
 			<nav className="w-full">
-				<h3 className="text-white w-fit">
+				<h1 className="text-white w-fit text-2xl">
 					<b>
 						<Link href='/'>
 							FindMyTokens
 						</Link>
 					</b>
-				</h3>
+				</h1>
 			</nav>
 			<div className="flex flex-col items-center justify-center md:p-12 min-h-screen h-full w-10/12 md:w-8/12">
-				<h1 className="my-5 text-white text-center text-4xl md:text-6xl"><b>Find Lost Tokens</b></h1>
+				<h2 className="my-5 text-white text-center text-4xl md:text-6xl"><b>Find Lost Tokens</b></h2>
 				<p className="my-5 text-white text-center text-base md:text-xl h-full">
 					FindMyTokens matches wallet transactions to exchange deposit addresses, aiding in account recovery, identifying unclaimed balances, and improving recordkeeping.
 				</p>
