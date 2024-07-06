1b3175fbdaf6a5289dec61823365e2f31fb8b516
dariovp <dariovp01@gmail.com>
2024-07-06 19:19:03 -0300

    Update robots gen file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
 		rules: {
 			userAgent: '*',
 			allow: '/',
-			disallow: '/addresses/',
+			disallow: '/return',
 		},
 		sitemap: 'https://findmytokens.com/sitemap.xml',
 	}
