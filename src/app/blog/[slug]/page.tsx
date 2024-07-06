e790d5bffbe0cca6fda499a5fa077de27af8e429
dariovp <dariovp01@gmail.com>
2024-07-06 20:33:02 -0300

    Add metadata generation for blogposts

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { notFound } from "next/navigation"
 import { POSTS_QUERY, POST_QUERY } from "../../../../sanity/lib/queries";
 import Post from "../../../components/blog/post"
 import { sanityFetch } from "../../../../sanity/lib/fetch"
+import { Metadata } from "next";
 
 
 export async function generateStaticParams() {
@@ -16,6 +17,30 @@ export async function generateStaticParams() {
 	return posts.map((post) => ({
 		slug: post.slug.current,
 	}))
+}
+
+export async function generateMetadata({ params }: { params: QueryParams }): Promise<Metadata> {
+	const post = await sanityFetch<SanityDocument>({ query: POST_QUERY, params })
+	if (!post) {
+		return notFound()
+	}
+	return {
+		title: post.title,
+		metadataBase: new URL("https://findmytokens.com/blog/"),
+		alternates: {
+			canonical: `https://findmytokens.com/blog/${post.slug.current}`,
+		},
+		icons: {
+			icon: '/favicon.ico',
+		},
+		openGraph: {
+			title: post.title,
+			description: post.description,
+			url: `https://findmytokens.com/blog/${post.slug.current}`,
+			siteName: 'FindMyTokens',
+			type: "article"
+		}
+	}
 }
 
 export default async function Page({ params }: { params: QueryParams }) {
