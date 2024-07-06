3884debe09ac84cb5c6e122a93524993da013bf1
dariovp <dariovp01@gmail.com>
2024-07-06 19:50:45 -0300

    Add /blog and /blog/* to sitemap generation. Closes #1

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,31 @@
 import { MetadataRoute } from 'next'
+import { client } from '../../sanity/lib/client'
 
-export default function sitemap(): MetadataRoute.Sitemap {
-	return [
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+	const staticPages = [
 		{
 			url: 'https://findmytokens.com',
 			lastModified: new Date(),
 			changeFrequency: 'yearly',
+		},
+		{
+			url: 'https://findmytokens.com/blog',
+			lastModified: new Date(),
+			changeFrequency: 'monthly',
 		}
 	]
+
+	const blogPosts = await client.fetch<any>(`*[_type == "post"] {
+		_id,
+		title,
+		slug
+	}`)
+
+	const dynamicPages = blogPosts.map((post: any) => ({
+		url: `https://findmytokens.com/blog/${post.slug.current}`,
+		lastModified: new Date(),
+		changeFrequency: 'monthly',
+	}))
+
+	return [...staticPages, ...dynamicPages];
 }
