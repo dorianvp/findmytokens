eda0f0bda881d8dd9810edff159e7830e66a202b
dariovp <dariovp01@gmail.com>
2024-07-06 19:12:21 -0300

    Fix post-list grid cols

--- src/components/blog/post-list.tsx
+++ src/components/blog/post-list.tsx
@@ -3,8 +3,8 @@ import { PostPreview } from "./post-preview";
 
 export function PostList({ posts }: { posts: Post[] }) {
 	return (
-		<section className="container grid grid-cols-1 divide-y divide-blue-100 p-0">
-			<ul className="grid grid-cols-1 md:grid-cols-4 gap-8">
+		<section className="container grid grid-cols-1 divide-y divide-blue-100 p-0 m-0">
+			<ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
 				{posts?.length > 0 ? (
 					posts.map((post) => (
 						<PostPreview post={post} key={post._id} />
